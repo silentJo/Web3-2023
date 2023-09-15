94edca8bfef0700e74e6c676ae3c764c45df0ebb
Jonathan CASIER <jonathancasier@gmail.com>
2023-09-15 11:23:09 +0200

    1.1-1.2 : import image

--- exercises/1.1-1.2/src/components/header/header.jsx
+++ exercises/1.1-1.2/src/components/header/header.jsx
@@ -1,6 +1,9 @@
+import logo from './logo.png';
+
 const Header = (props) => {
     return (
         <div>
+            <img src={logo} alt="Logo" />
             <h1>{props.name}</h1>
         </div>
     )
