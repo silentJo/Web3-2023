68e74bab4f7c2b43d8d5636022ab58512f59a570
Jonathan CASIER <jonathancasier@gmail.com>
2023-09-15 11:01:29 +0200

    1.1-1.2 : refactor - modules

--- exercises/1.1-1.2/src/App.jsx
+++ exercises/1.1-1.2/src/App.jsx
@@ -1,44 +1,13 @@
-const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.name}</h1>
-        </div>
-    )
-}
-
-const Content = (props) => {
-    return (
-        <>
-            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-        </>
-    )
-}
-
-const Part = (props) => {
-    return (
-        <div>
-            <p>{props.name} {props.exercises}</p>
-        </div>
-    )
-}
-
-const Total = (props) => {
-    return (
-        <div>
-            <p>Number of exercises {props.total}</p>
-        </div>
-    )
-}
-
+import Header from "./components/header/header.jsx";
+import Content from "./components/content/content.jsx";
+import Total from "./components/total/total.jsx";
 
 const App = () => {
     const name = 'Half Stack application development'
     const parts = [
         { name : 'Fundamentals of React', exercises : 10 },
-        { name : 'Using props to pass data', exercises: 7 },
-        { name : 'State of a component', exercises: 14 }
+        { name : 'Using props to pass data', exercises: 10 },
+        { name : 'State of a component', exercises: 15 }
     ]
     const total= parts[0].exercises + parts[1].exercises + parts[2].exercises
 
---
+++ exercises/1.1-1.2/src/components/header/header.jsx
@@ -0,0 +1,9 @@
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+        </div>
+    )
+}
+
+export default Header
