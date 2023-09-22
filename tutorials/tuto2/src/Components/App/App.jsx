3252d5cd42705b1474ab05e75703b85e870eee33
Jonathan CASIER <jonathancasier@gmail.com>
2023-09-22 11:29:29 +0200

    tuto 2

---
+++ tutorials/tuto2/src/Components/App/App.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react";
+import Display from '../Display/Display.jsx'
+import Button from "../Button/Button.jsx";
+
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button
+                handleClick={increaseByOne}
+                text='plus'
+            />
+            <Button
+                handleClick={setToZero}
+                text='zero'
+            />
+            <Button
+                handleClick={decreaseByOne}
+                text='minus'
+            />
+        </div>
+    )
+}
+
+export default App
---
+++ tutorials/tuto2/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './Components/App/App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
