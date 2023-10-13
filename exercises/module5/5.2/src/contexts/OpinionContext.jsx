f5b401d1d383a3d449451d4b2c4803f39e70908f
Jonathan CASIER <jonathancasier@gmail.com>
2023-10-13 15:07:34 +0200

    module5 : add alerts

--- exercises/module5/5.2/src/contexts/OpinionContext.jsx
+++ exercises/module5/5.2/src/contexts/OpinionContext.jsx
@@ -3,12 +3,13 @@ import React, {useState} from "react"
 const OpinionContext = React.createContext(null)
 
 const ProviderWrapper = (props) => {
+    
     const [opinions, setOpinions] = useState([])
 
     const voteForOpinion = (id) => {
         const index = opinions.findIndex((opinion) => opinion.id === id)
         if (index === -1) {
-            alert("opinion with provided id does not exist")
+            alert("Provided id does not exist")
             return
         }
         const opinion = opinions[index]
@@ -20,10 +21,15 @@ const ProviderWrapper = (props) => {
         newOpinions[index] = newOpinion
         setOpinions(newOpinions)
     }
+
     const addOpinion = (label) => {
-        const labelExists = opinions.some((opinion) => opinion.label === label)
+        const labelExists = opinions.some((opinion) => opinion.label.toLowerCase() === label.toLowerCase())
         if(labelExists) {
             alert("this opinion is already added")
+            return
+        }
+        if(label.trim()==='') {
+            alert("label can't be empty")
             return
         }
         const newOpinion = {
@@ -35,17 +41,12 @@ const ProviderWrapper = (props) => {
         setOpinions(newOpinions)
     }
 
-    const reset = () => {
-        setOpinions([])
-    }
-
     const sortedOpinions = opinions.sort((a, b) => b.score - a.score)
 
     const exposedValue = {
-        sortedOpinions,
         voteForOpinion,
         addOpinion,
-        reset
+        sortedOpinions
     }
 
     return (
